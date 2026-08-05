4c91cf19f25f49a20cfb35a5154a73023b5c53cf
RVMarker <nuria.ledezma95@gmail.com>
2026-08-05 14:25:52 -0600

    V11: expose financial analysis integrator

---
+++ src/investment_analyzer/analysis/integration/__init__.py
@@ -0,0 +1,5 @@
+"""Integration boundaries between analysis engines."""
+
+from .fundamental_valuation_risk import FinancialAnalysisIntegrator, IntegratedFinancialAnalysis
+
+__all__ = ["FinancialAnalysisIntegrator", "IntegratedFinancialAnalysis"]
