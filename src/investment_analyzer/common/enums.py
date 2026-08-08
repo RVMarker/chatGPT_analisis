83ef429f274f9182aace94584a526725b29949f9
RVMarker <nuria.ledezma95@gmail.com>
2026-08-08 16:12:06 -0600

    V12.12 expand supported asset types: FIBRA and BOND

--- src/investment_analyzer/common/enums.py
+++ src/investment_analyzer/common/enums.py
@@ -1,13 +1,11 @@
 from enum import Enum
 
-
 class Decision(Enum):
     BUY = "BUY"
     ACCUMULATE = "ACCUMULATE"
     HOLD = "HOLD"
     REDUCE = "REDUCE"
     SELL = "SELL"
-
 
 class Provider(Enum):
     YAHOO = "Yahoo Finance"
@@ -16,22 +14,21 @@ class Provider(Enum):
     POLYGON = "Polygon"
     FINNHUB = "Finnhub"
 
-
 class AssetType(Enum):
     STOCK = "Stock"
     ETF = "ETF"
     REIT = "REIT"
+    FIBRA = "FIBRA"
     CRYPTO = "Crypto"
+    BOND = "Bond"
     INDEX = "Index"
     FOREX = "Forex"
-
 
 class Currency(Enum):
     USD = "USD"
     MXN = "MXN"
     EUR = "EUR"
     GBP = "GBP"
-
 
 class Exchange(Enum):
     NASDAQ = "NASDAQ"
@@ -40,20 +37,17 @@ class Exchange(Enum):
     BIVA = "BIVA"
     CME = "CME"
 
-
 class ReportFormat(Enum):
     HTML = "html"
     PDF = "pdf"
     DOCX = "docx"
     JSON = "json"
 
-
 class ConfidenceLevel(Enum):
     LOW = "LOW"
     MEDIUM = "MEDIUM"
     HIGH = "HIGH"
     VERY_HIGH = "VERY_HIGH"
-
 
 class AnalysisStatus(Enum):
     SUCCESS = "SUCCESS"
